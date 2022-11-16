8907a53d1d0be2163c8349032c3fece309f2476e
Duran1360 <marioduran260@gmail.com>
2022-11-16 11:36:49 -0400

    exercise 4 realiced

--- 04.js
+++ 04.js
@@ -6,13 +6,20 @@
 */
 
 function numeroSimetrico(num) {
-  // La funcion llamada 'numeroSimetrico' recibe como argumento un numero entero 'num'
+  // La funcion llamada 'numeroSimetrico' recibe como argumento un numero entero 'numero'
   // Esta devuelve true o false dependiendo de si el número es simétrico o no. 
   // Un número es simétrico cuando es igual a su reverso.
   // Ej:
   // numeroSimetrico(11711) devuelve true
 
   // Tu código:
+  var numero=String(num);
+  var reverso="";
+  for(var i=numero.length-1;i>=0;i--)
+  {
+    reverso+=numero[i];
+  }
+  return numero===reverso;
 
 }
 
